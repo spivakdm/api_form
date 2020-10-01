84fef3caaa41828e6a466817eb21f047b9a04ac9
spivakdm <spivak.dm@gmail.com>
2020-10-02 01:06:55 +0300

    Create register1_form.php

---
+++ register1_form.php
@@ -0,0 +1,10 @@
+<div id="content" class = "form0" role="main">
+    <form id = "submitform1" method="post">
+        <label for="uname">name:</label><br>
+        <input type="text" id="uname" name="uname"><br>
+        <label for="uphone">phone:</label><br>
+        <input type="number" id="uphone" name="uphone"><br><br>
+        <input type="submit" name = "submitform1">
+    </form>
+
+</div>
